c867a26737dcbd1adead166ba36eac2b608af15f
Kauã Felipe <128815079+KauaJenior@users.noreply.github.com>
2025-08-27 11:35:12 -0300

    Projeto Vendas

---
+++ Proj. MVC Vendedores/Proj. MVC Vendedores/Venda.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj._MVC_Vendedores
+{
+    internal class Venda
+    {
+        private int qtde;
+        private double valor;
+
+        public int Qtde { get => qtde; set => qtde = value; }
+        public double Valor { get => valor; set => valor = value; }
+
+
+        public Venda(int qtde, double valor) 
+        { 
+          this.qtde = qtde;
+            this.valor = valor;
+        }
+
+        public double valorMedia()
+        {
+         return valor * qtde;  
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Proj. MVC Vendedores/Proj. MVC Vendedores/Vendedor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj._MVC_Vendedores
+{
+    internal class Vendedor
+    {
+        private int id;
+        private string nome;
+        private double percComissao;
+        private Venda[] asVendas;
+
+        public int Id { get => id; set => id = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public double PercComissao { get => percComissao; set => percComissao = value; }
+        internal Venda[] AsVendas { get => asVendas; }
+
+        public Vendedor(int id, string nome, double percComissao)
+
+        {
+            this.id = id;
+            this.nome = nome;
+            this.percComissao = percComissao;
+            this.asVendas = new Venda[31];
+        }
+
+        public Vendedor() { }
+
+        public void registrarVenda(int dia, Venda venda)
+        {
+            {
+
+        }
+    }
+}
